afd2faaeae20fc89a33d9412831dc411ef94ff86
Leonardo <leonardo.stenio@gmail.com>
2022-11-08 00:13:25 -0300

    atualizando tipagem do tema

--- App.tsx
+++ App.tsx
@@ -1,13 +1,20 @@
+import { Loading } from "@components/Loading";
 import { ThemeProvider } from "styled-components";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 
 import theme from "./src/theme";
 
 import Groups from "@screens/Groups";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
